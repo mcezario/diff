90d872feb5f1869acb90213f2275e94607bd21b8
Maikon <maikon.cezario@gmail.com>
2019-08-29 15:51:57 -0400

    Removed unnecessary annotation at the moment.

--- src/main/java/org/mcezario/diff/DiffApplication.java
+++ src/main/java/org/mcezario/diff/DiffApplication.java
@@ -2,10 +2,8 @@ package org.mcezario.diff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class DiffApplication {
 
 	public static void main(String[] args) {
